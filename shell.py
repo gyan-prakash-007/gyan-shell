5b3964caf5dd4e8a9b3beea44b35f9421e4bdb7b
gyan-prakash-007 <gyansir22@gmail.com>
2026-07-06 22:49:59 +0530

    Fixed the error message to show full command

--- shell.py
+++ shell.py
@@ -1,4 +1,5 @@
 import subprocess
+import os 
 
 def main():
     while True :
@@ -7,9 +8,20 @@ def main():
         if command.strip() == "":
             continue
 
-            parts = command.split()
-            subprocesses.run(parts)
+        parts = command.split()
 
+        try:
+            if parts[0] == "cd":
+                target_dir = parts[1] if len(parts) >1 else os.path.expanduser("~")
+                os.chdir(target_dir)
+
+            else:
+             subprocess.run(parts)
+        except FileNotFoundError:
+            print(f"gyan-shell: command not found or no such file/directory: {command}")
+        except Exception as e :
+            print(f"gyan-shell: error {e}")
+         
 
 if __name__ == "__main__":
     main()
